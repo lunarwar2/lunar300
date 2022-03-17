ef158619a98d1d0a82a76d4ff62a11a3d2a6b108
lunarwar2 <101807623+lunarwar2@users.noreply.github.com>
2022-03-17 19:24:53 +0700

    Create telungatus.js

---
+++ telungatus.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '00d619809c34d55d95d66030ea83a1a4',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://snailteasesimulator.com/00d619809c34d55d95d66030ea83a1a4/invoke.js"></scr' + 'ipt>');
